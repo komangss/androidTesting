4ee2e33015f08bb38141a181ee86c86c6a407709
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-10-09 19:04:38 +0800

    7 - dagger hilt preparation

---
+++ app/src/main/java/com/komangss/androidtesting/ShoppingApplication.kt
@@ -0,0 +1,9 @@
+package com.komangss.androidtesting
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShoppingApplication : Application() {
+
+}
---
+++ app/src/main/java/com/komangss/androidtesting/other/Constants.kt
@@ -0,0 +1,7 @@
+package com.komangss.androidtesting.other
+
+object Constants {
+    const val DATABASE_NAME = "shopping_database"
+    const val BASE_URL = "https://pixabay.com"
+
+}
